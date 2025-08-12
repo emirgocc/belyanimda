52354a6147587e5b4a4992822e203c94979706c3
emirgocc <emirgoc_39@hotmail.com>
2025-08-12 15:14:23 +0300

    notif update

--- mobile/screens/NotificationsScreen.js
+++ mobile/screens/NotificationsScreen.js
@@ -206,7 +206,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'transparent',
-    paddingBottom: 90, // Floating navigation için alan bırak
+    paddingBottom: 60, // Floating navigation için alan bırak
   },
   header: {
     backgroundColor: 'transparent',
